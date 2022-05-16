085f5a191f267025f30c469a94b0a126f6b2d347
John Arney <Jarney903@gmail.com>
2022-05-16 16:57:14 -0500

    Completed Module #11.3

---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+// 11.2.4
+// import the data from data.js
+// Use const becase we do not want the data variable to be reassigned or reused
+const tableData = data;
+
+// D3 is a JavaScript library that produces sophisticated and highly dynamic graphics in an HTML webpage.
+// Reference the HTML table using d3
+// Use d3.select to tell JavaScript to look for the <tbody> tags in the HTML
+var tbody = d3.select("tbody");
+
+
