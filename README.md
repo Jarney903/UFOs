a759e519eb4e13b453215423f55495225ae1bf90
John Arney <Jarney903@gmail.com>
2022-05-18 22:31:25 -0500

    Completed

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# UFOs
-Module #11
+
+# UFO Finder Site
+
+## Project Overview
+Create a webpage with a dynamic tableto provide a more in-depth analysis of UFO sightings by allowing users to filter for multiple criteria at the same time. In addition to the date, users can filter the UFO sightings table by the city, state, country, and shape.
+
+## Resources
+- Data Source: data.js
+- Software: JavaScript, HTML, VSCode, D3, Bootstrap
+
+## UFO Finder Results
+The improved search criteria works as follows:
+- Select first search criteria by selecting the filter search box of choice and inputting criteria
+- See Figure 1 for an example of entering the filter state (tx) as the first filter
+- To add more search criteria, simply continue to add more filters in other filter search boxes
+- See Figure 2 for an example of entering the filter state (tx) and the filter date (1/8/2010)
+- The user can add up to 5 filters, including date, city, state, country, and shape. 
+
+### Figure 1: State Filter
+![Texas](https://github.com/Jarney903/UFOs/blob/main/Resources/screenshot1.png)
+<br />
+
+### Figure 2: State Filter + Date Filter
+![Texas on 1/8/2010](https://github.com/Jarney903/UFOs/blob/main/Resources/screenshot2.png)
+<br />
+
+## UFO Finder Summary
+One drawback of this new design is that the filter search fields are ridgid. If the dates, cities, states, country, or shapes are not input exactly as they appear in the database, the filter will not return them. This is ok for states and dates, but limits the functionality of filtering by shape drastically. 
+<br />
+Two recommendations for further development are as follows.
+   -  Improve flexibility of the filter inputs.
+   -  Improve database to include more up to date sightings.
--- static/js/app.js
+++ static/js/app.js
@@ -1,66 +1,71 @@
-// 11.2.4
-// import the data from data.js
-// Use const becase we do not want the data variable to be reassigned or reused
+// from data.js
 const tableData = data;
 
-// D3 is a JavaScript library that produces sophisticated and highly dynamic graphics in an HTML webpage.
-// Reference the HTML table using d3
-// Use d3.select to tell JavaScript to look for the <tbody> tags in the HTML
+// get table references
 var tbody = d3.select("tbody");
-// 11.5.2 
-// Examlple using forEach
-// let numbers = [1,2,3,4,5,6];
-// numbers.forEach(function (elements) {console.log(elements**2);})
-// 11.5.1 Building a Table
 
 function buildTable(data) {
-    // First, clear out any existing data
-    tbody.html("");
-  
-    // Next, loop through each object in the data
-    // and append a row and cells for each value in the row
-    data.forEach((dataRow) => {
-      // Append a row to the table body
-      let row = tbody.append("tr");
-  
-      // Loop through each field in the dataRow and add
-      // each value as a table cell (td)
-      Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
-        }
-      );
-    });
-  }
+  // First, clear out any existing data
+  tbody.html("");
 
-// 11.5.3 & 11.5.4
-function handleClick() {
-    // Grab the datetime value from the filter
-    let date = d3.select("#datetime").property("value");
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+    });
+  });
+}
+
+// 1. Create a variable to keep track of all the filters as an object.
+let filters = {};
+
+// 3. Use this function to update the filters. 
+function updateFilters() {
+
+    // 4a. Save the element that was changed as a variable.
+    let changedElement = d3.select(this).select('input');
+    // 4b. Save the value that was changed as a variable.
+    let changedValue = changedElement.property("value");
+    // 4c. Save the id of the filter that was changed as a variable.
+    let filterID = changedElement.attr("id");
+  
+    // 5. If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object.
+    if (changedValue){
+      filters[filterID] = changedValue;
+    }
+      else {
+        delete filters[filterID]};
+  
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable();
+  }
+  
+  // 7. Use this function to filter the table when data is entered.
+  function filterTable() {
+  
+    // 8. Set the filtered data to the tableData.
     let filteredData = tableData;
   
-     // Check to see if a date was entered and filter the
-    // data using that date.
-    if (date) {
-      // Apply `filter` to the table data to only keep the
-      // rows where the `datetime` value matches the filter value
-      filteredData = filteredData.filter(row => row.datetime === date);
-    };
+    // 9. Loop through all of the filters and keep any data that
+    // matches the filter values
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
   
-     // Rebuild the table using the filtered data
-    // @NOTE: If no date was entered, then filteredData will
-    // just be the original tableData.
+    // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
-  };
-
-// Data-Driven Documents (D3 for short) is a JavaScript library that adds interactive functionality, 
-// such as when users click a button to filter a table. 
-//  It works by "listening" for events, such as a button click, then reacts according to the code we've created.
-
-// 11.5.4 
-d3.selectAll("#filter-btn").on("click", handleClick);
-
-// Build the table when the page loads
-buildTable(tableData);
-
-
+  }
+  
+  // 2. Attach an event to listen for changes to each filter
+  d3.selectAll(".list-group-item").on("change", updateFilters);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
